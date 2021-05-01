4a3fc56bb9053910c26b856b6132aa247addf2bb
Spencer <vecna@Agrippa.localdomain>
2021-05-01 14:54:31 -0700

    implemented basic response info

--- mysite/treeID/views.py
+++ mysite/treeID/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.db import connection
 from django.shortcuts import render
 from .forms import QueryForm
+from django.template.response import TemplateResponse
 
 def get_query(request):
     # if this is a POST request we need to process the form data
@@ -24,7 +25,16 @@ def get_query(request):
 
 def index(request):
     ID = str(request.POST.get('query'))
-    query = "SELECT * FROM treedata WHERE tree_id='"+ID+"';"
-    cursor = connection.cursor()
-    cursor.execute(query)
-    return HttpResponse(cursor.fetchall())
+    fields_to_query = ["id","group_", "leaf_fall", "name", "genus", "species_name", "family", "age_min", "age_max", "height_min", "height_max", "comments"]
+    context_dict = {}
+    for column in fields_to_query:
+        query = "SELECT "+column+" FROM tree_data_cleaned WHERE id=%s;"
+        cursor = connection.cursor()
+        cursor.execute(query, [ID])
+        query_response = cursor.fetchall()
+        if len(query_response) != 1 or len(query_response[0]) != 1:
+            raise ValueError('query response malformed')
+        response = query_response[0][0]
+        context_dict[column] = response
+    return TemplateResponse(request, 'ID_response.html', context_dict)
+
