e31cc2e8737a8dd9a8508a0ac9fe796732181ab3
Tristan Chung <swit.chung@gmail.com>
2021-10-06 14:02:56 -0700

    input validation using regular expressions as well as url redirects in cases of invalid inputs

--- mysite/treeID/views.py
+++ mysite/treeID/views.py
@@ -1,5 +1,6 @@
 from mysite.settings import BASE_DIR
 from treeID.models import TreeDataFinal
+from mysite.settings import MEDIA_ROOT
 from treeID.models import Comment
 from django.http import HttpResponseRedirect
 from django.db import connection
@@ -9,6 +10,8 @@ from .forms import CommentForm
 from django.template.response import TemplateResponse
 from django.forms.models import model_to_dict
 
+import re
+from PIL import Image
 
 def redirect(request):
     return HttpResponseRedirect('/treeID/query/')
@@ -49,14 +52,23 @@ def get_comment(request):
 
     return render(request, 'comment.html', {'form': form})
 
+
 def index(request):
     ID = str(request.GET.get('query'))
     columns = ["id", "group_field", "leaf_fall", "name", "genus", "species_name", "family", "age_min", "age_max", "height_min", "height_max"]
-    tree = TreeDataFinal.objects.get(id=ID)
+    ID = ID.capitalize()
+    checkID = re.fullmatch('[A-Z]{1}[0-9]{1,3}', ID)
+    if not checkID:
+        return render(request, 'invalid_ID.html')
+    try:
+        tree = TreeDataFinal.objects.get(id=ID)
+    except:
+        return render(request, 'invalid_ID.html')
     context_dict = {}
     values = model_to_dict(tree)
     for field in columns:
-        context_dict[field] = values[field]
+            context_dict[field] = values[field]
+ 
     comments = Comment.objects.all()
     context = {
             'context_dict': context_dict,
@@ -66,13 +78,25 @@ def index(request):
 
 def comment_handler(request):
     treeID = str(request.POST.get('ID'))
+    checkID = re.fullmatch('[A-Z]{1}[0-9]{1,3}', treeID)
+   
+    if not checkID:
+        return render(request, 'invalid_comment.html')
     comment_text = str(request.POST.get('comment'))
+    checkComment = re.fullmatch("^[a-zA-Z0-9 ,.?!-]*$", comment_text)
+    if not checkComment:
+        return render(request, 'invalid_comment.html')
     can_contact = request.POST.get('can_contact')
     if can_contact == "on":
         can_contact = True
     else:
         can_contact = False
     contact_info = request.POST.get('contact_info')
+    checkEmail = re.fullmatch('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', contact_info)
+    if can_contact:
+        if not checkEmail:
+            return render(request, 'invalid_comment.html')
+
     save = request.POST.get('save')
     comment = Comment()
     comment.treeID = treeID
@@ -81,8 +105,15 @@ def comment_handler(request):
     comment.contact_info= contact_info
     comment.approval = False
 
+
     if len(request.FILES) == 1:
         comment.photo= request.FILES["photo"]
+    print(comment.photo)
+    
+    img = Image.open(MEDIA_ROOT+'/tree_photos/'+str(comment.photo))
+    img_resize = img.resize((1980, 1080))
+    img_resize.save(MEDIA_ROOT+'/tree_photos/'+str(comment.photo))
+
     if save:
         comment.save()
     return HttpResponseRedirect('/treeID/query/')
